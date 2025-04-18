f2b624c5080cfcb2c3fa0b9ac353316a3c5f8317
Dvdrepetto <davidmrepetto@gmail.com>
2025-04-18 12:55:39 +0300

    Refactor: update Contact page with social links and enhance Intro component description

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,14 +7,27 @@ export default function Intro() {
       <div className='mt-2 flex-1 md:mt-0'>
         <h1 className='title no-underline'>Hey, I&apos;m David.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I&apos;m a web developer based in Marbella, Spain.<br />
-          During my time at the Ironhack bootcamp, I developed strong skills in modern technologies and agile methodologies, enabling me to build high-quality web applications.<br />
-          My previous training in JavaScript at Coderhouse further strengthened my foundation and problem-solving abilities in front-end development.<br />
-          I&apos;m proficient in HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB, and I have experience working with Agile methodologies and Git version control.<br />
-          Before transitioning into tech, I developed teamwork and adaptability in fast-paced environments during buffet and kitchen services.<br />
-          My short-term goal is to continue enhancing my technical expertise and problem-solving skills as a Full Stack Developer on innovative projects.<br />
-          Outside of technology, my lifelong passion for football has taught me the value of teamwork, leadership, and strategic decision-making—qualities I bring to every professional endeavor.
-        </p>
+  <span className='hover:text-white hover:brightness-125 transition duration-300'>
+    I&apos;m a web developer passionate about building meaningful web experiences.
+  </span>
+  <br />
+  <span className='hover:text-white hover:brightness-125 transition duration-300'>
+    I love building web apps that solve real problems. I’ve trained at Ironhack and Coderhouse, where I learned to work with modern technologies like React, Node.js, and MongoDB, always with an Agile mindset.
+  </span>
+  <br />
+  <span className='hover:text-white hover:brightness-125 transition duration-300'>
+    Before switching to tech, I worked in fast-paced environments like kitchens and buffets — that's where I picked up teamwork, adaptability, and hustle.
+  </span>
+  <br />
+  <span className='hover:text-white hover:brightness-125 transition duration-300'>
+    Now I’m looking to grow as a Full Stack Developer, keep learning, and join projects that make a real impact.
+  </span>
+  <br />
+  <span className='hover:text-white hover:brightness-125 transition duration-300'>
+    Off the clock, football keeps me sharp — it’s taught me about strategy, leadership, and playing for the team.
+  </span>
+</p>
+
       </div>
       <div className='relative'>
         <Image
